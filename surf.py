ebc221a82dd848dd41d74294003f46d5ffad8998
murillo.costa@live.com <murillo.costa@live.com>
2021-12-15 12:10:46 -0300

    Update surf.py

--- surf.py
+++ surf.py
@@ -1,11 +1,21 @@
 import requests
+import time
 from bs4 import BeautifulSoup
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 lista_praia = ['torrefacao', 'espanhol', 'paciencia', 'farol-da-barra', 'barravento', 'tonys', 'praia-da-onda', 'pescador', 'secret-2', 'stella', 'aleluia',]
 
+for praia in lista_praia:
+    url = f'https://www.waves.com.br/surf/ondas/condicao/bahia/{praia}'
 
+option = Options()
+option.headless = True
+driver = webdriver.Chrome()
+
+driver.get(url)
+time.sleep(5)
 
 for praia in lista_praia:
     htmldata = requests.get(
@@ -23,4 +33,8 @@ for praia in lista_praia:
         print(f'Tamanho da onda: {onda_tamanho.text.strip()}')
     else:
         print(f'Tamanho da onda: {onda_tamanho.text.strip()[:4]}m')
+
+element_wave_size = driver.find_element_by_id('forecast_wave_size')
+html_content = element_wave_size.get_attribute('outerHTML')
+print(html_content)
        
